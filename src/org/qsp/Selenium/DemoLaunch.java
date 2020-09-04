1ed23e2df4668328afaab7ca6e592e02ce5d07b4
Raman <Raman@DESKTOP-9P221R1>
2020-09-05 03:01:19 +0530

    1st webLanch

--- src/org/qsp/Selenium/DemoLaunch.java
+++ src/org/qsp/Selenium/DemoLaunch.java
@@ -1,5 +1,6 @@
 package org.qsp.Selenium;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoLaunch {
@@ -7,7 +8,9 @@ public class DemoLaunch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.setProperty("webdriver.chrome.driver",".\\software\\chromedriver.exe" );
-      ChromeDriver driver = new ChromeDriver();
+      WebDriver driver = new ChromeDriver();
+      driver.get("https://www.facebook.com/");
+      driver.close();
 	}
 
 }
