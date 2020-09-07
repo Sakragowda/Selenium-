cfede5fff81854d0712f584e62694fbb0c028096
Raman <Raman@DESKTOP-9P221R1>
2020-09-07 22:37:40 +0530

    naukari JOB

--- src/org/qsp/Selenium/DemoLaunch.java
+++ src/org/qsp/Selenium/DemoLaunch.java
@@ -5,13 +5,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoLaunch {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
       System.setProperty("webdriver.chrome.driver",".\\software\\chromedriver.exe" );
       WebDriver driver = new ChromeDriver();
-      driver.get("https://www.facebook.com/");
+      driver.manage().window().maximize();
+      driver.get("https://www.google.com/");
+ //     driver.navigate().back();
+ //     Thread.sleep(2000);
+ //     driver.navigate().forward();
+      Thread.sleep(200);
+      driver.navigate().to("https://www.Amazon.in/");
+      Thread.sleep(200);
+      String title = driver.getTitle();
+      System.out.println(title);
+      System.out.println(driver.getCurrentUrl());
+      // System.out.println(driver.getPageSource());
+      System.out.println(driver.getWindowHandle());
       driver.close();
-      System.out.println("web is launching Succesfully");
+      //driver.quit();
+      System.out.println("WebSite launch Successfully");
 	}
-
 }
