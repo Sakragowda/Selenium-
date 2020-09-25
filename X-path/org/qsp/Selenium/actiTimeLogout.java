aa723fbb3bb2730d86e8485e5b0826b82e0311cc
Raman <Raman@DESKTOP-9P221R1>
2020-09-25 08:26:43 +0530

    x-path 2

--- X-path/org/qsp/Selenium/actiTimeLogout.java
+++ X-path/org/qsp/Selenium/actiTimeLogout.java
@@ -14,6 +14,8 @@ public class actiTimeLogout {
 	      driver.manage().window().maximize();        // Maximise the window
 	      driver.get("https://demo.actitime.com/login.do");	
 	      
+	      Thread.sleep(2000);
+	      
 	      driver.findElement(By.xpath("//input[@id='username']")).sendKeys("admin");  // User Name
 	      driver.findElement(By.xpath("//input[@name='pwd']")).sendKeys("manager");   // Pass word
 	      driver.findElement(By.xpath("//a[@id='loginButton']/div[contains(.,'Login')]")).click(); // Login
@@ -23,9 +25,7 @@ public class actiTimeLogout {
 	      driver.findElement(By.xpath("//div[@id='popup_menu_support_createAccountColleague']")).click();  // Creat AC  
 		  Thread.sleep(2000);
 		  driver.findElement(By.xpath("//a[@class='logout']")).click();   // Logout
-		  //Thread.sleep(1000);
-		 // driver.close();
-		  //div[@class='menu_icon']/ancestor::div[@class='popup_menu_button popup_menu_button_support popup_menu_button_pressed']
+
 	}
 
 }
